ef79d7246db2d3659943d8a55715850fa6e5bd02
Quincy Morgan <2046746+quincylvania@users.noreply.github.com>
2025-12-10 13:24:58 -0500

    Organize sql

--- server/update_sql_functions.sh
+++ server/update_sql_functions.sh
@@ -12,7 +12,7 @@ DB_USER="osmuser"
 
 # area layer
 
-AREA_SQL_FUNCTION=$(<"$APP_DIR/sql/function_get_area_features_for_tile.sql")
+AREA_SQL_FUNCTION=$(<"$APP_DIR/sql/functions/get_area_features_for_tile.sql")
 
 AREA_KEY_LIST=$(grep -v '^$' "$APP_DIR/schema_data/area_key.txt" | sed "s/.*/'&'/" | paste -sd, -)
 AREA_SQL_FUNCTION=${AREA_SQL_FUNCTION//\{\{AREA_KEY_LIST\}\}/$AREA_KEY_LIST}
@@ -25,7 +25,7 @@ AREA_SQL_FUNCTION=${AREA_SQL_FUNCTION//\{\{LOW_ZOOM_AREA_KEY_LIST\}\}/$LOW_ZOOM_
 
 # line layer
 
-LINE_SQL_FUNCTION=$(<"$APP_DIR/sql/function_get_line_features_for_tile.sql")
+LINE_SQL_FUNCTION=$(<"$APP_DIR/sql/functions/get_line_features_for_tile.sql")
 
 LINE_KEY_LIST=$(grep -v '^$' "$APP_DIR/schema_data/line_key.txt" | sed "s/.*/'&'/" | paste -sd, -)
 LINE_SQL_FUNCTION=${LINE_SQL_FUNCTION//\{\{LINE_KEY_LIST\}\}/$LINE_KEY_LIST}
@@ -38,7 +38,7 @@ LINE_SQL_FUNCTION=${LINE_SQL_FUNCTION//\{\{LOW_ZOOM_LINE_KEY_LIST\}\}/$LOW_ZOOM_
 
 # point layer
 
-POINT_SQL_FUNCTION=$(<"$APP_DIR/sql/function_get_point_features_for_tile.sql")
+POINT_SQL_FUNCTION=$(<"$APP_DIR/sql/functions/get_point_features_for_tile.sql")
 
 POINT_KEY_LIST=$(grep -v '^$' "$APP_DIR/schema_data/point_key.txt" | sed "s/.*/'&'/" | paste -sd, -)
 POINT_SQL_FUNCTION=${POINT_SQL_FUNCTION//\{\{POINT_KEY_LIST\}\}/$POINT_KEY_LIST}
@@ -48,7 +48,7 @@ POINT_SQL_FUNCTION=${POINT_SQL_FUNCTION//\{\{POINT_KEY_PREFIX_LIKE_STATEMENTS\}\
 
 # relation layer
 
-ROOT_SQL_FUNCTION=$(<"$APP_DIR/sql/function_get_beefsteak_tile.sql")
+ROOT_SQL_FUNCTION=$(<"$APP_DIR/sql/functions/get_beefsteak_tile.sql")
 
 RELATION_KEY_LIST=$(grep -v '^$' "$APP_DIR/schema_data/relation_key.txt" | sed "s/.*/'&'/" | paste -sd, -)
 ROOT_SQL_FUNCTION=${ROOT_SQL_FUNCTION//\{\{RELATION_KEY_LIST\}\}/$RELATION_KEY_LIST}
@@ -57,7 +57,7 @@ RELATION_KEY_PREFIX_LIKE_STATEMENTS=$(grep -v '^$' "$APP_DIR/schema_data/relatio
 ROOT_SQL_FUNCTION=${ROOT_SQL_FUNCTION//\{\{RELATION_KEY_PREFIX_LIKE_STATEMENTS\}\}/$RELATION_KEY_PREFIX_LIKE_STATEMENTS}
 
 
-sudo -u "$DB_USER" psql "$DB_NAME" -v ON_ERROR_STOP=1 -f "$APP_DIR/sql/function_get_ocean_for_tile.sql"
+sudo -u "$DB_USER" psql "$DB_NAME" -v ON_ERROR_STOP=1 -f "$APP_DIR/sql/functions/get_ocean_for_tile.sql"
 sudo -u "$DB_USER" psql "$DB_NAME" -v ON_ERROR_STOP=1 <<< "$AREA_SQL_FUNCTION"
 sudo -u "$DB_USER" psql "$DB_NAME" -v ON_ERROR_STOP=1 <<< "$LINE_SQL_FUNCTION"
 sudo -u "$DB_USER" psql "$DB_NAME" -v ON_ERROR_STOP=1 <<< "$POINT_SQL_FUNCTION"
