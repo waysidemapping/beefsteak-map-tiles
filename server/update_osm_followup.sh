435e74ee3d7624511cbeecafc8e28d23b7b7f6d8
Quincy Morgan <2046746+quincylvania@users.noreply.github.com>
2026-01-10 23:57:27 -0500

    sudo not needed since already running as user

--- server/update_osm_followup.sh
+++ server/update_osm_followup.sh
@@ -7,8 +7,7 @@ set -euo pipefail
 SCRIPT_DIR="$(dirname "$0")"
 
 DB_NAME="osm"
-DB_USER="osmuser"
 
 echo "Running post-import SQL queries..."
-sudo -u "$DB_USER" psql "$DB_NAME" \
+psql "$DB_NAME" \
     --file="$SCRIPT_DIR/sql/post_init_or_update/area_relation.sql"
