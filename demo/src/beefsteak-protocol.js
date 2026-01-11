4c19c352e6527a46333489812a792be8630a382c
Quincy Morgan <2046746+quincylvania@users.noreply.github.com>
2026-01-11 12:05:58 -0500

    Improve beefsteak-protocol performance

--- demo/src/beefsteak-protocol.js
+++ demo/src/beefsteak-protocol.js
@@ -17,66 +17,87 @@ function processIncomingBeefsteakTile(tile) {
   const relationCount = relationLayer?.length;
   if (!(relationCount > 0)) return tile;
   const allRelationKeys = relationLayer._keys;
-  let relationsById = {};
+  const allRelationKeysCount = allRelationKeys.length;
+  const relationsById = {};
   for (let i = 0; i < relationCount; i += 1) {
     let relation = relationLayer.feature(i);
     relationsById[Math.floor(relation.id * 0.1)] = relation;
   }
-  return {
-    layers: Object.entries(tile.layers)
-      .reduce((acc, [layerId, layer]) => ({
-      ...acc,
-      [layerId]: {
-        ...layer,
-        feature: (index) => {
-          const feature = layer.feature(index);
+  
+  for (const layerId in tile.layers) {
+    const layer = tile.layers[layerId];
+    tile.layers[layerId] = {
+      ...layer,
+      feature: getWrappedFeatureFunction(layer)
+    };
+  }
+  return tile;
 
-          if (feature.id) {
-            feature.properties['osm.id'] = osmIdFromBeefsteakFeatureId(feature.id);
-            feature.properties['osm.type'] = osmTypeFromBeefsteakFeatureId(feature.id);
-          }
+  function getWrappedFeatureFunction(layer) {
+    return function wrappedFeatureFunction(index) {
+      const feature = layer.feature(index);
+      const featureProperties = feature.properties;
 
-          if (feature.id % 10 === 3) { // relation
-            if (Object.keys(feature.properties).length === 0) {
-              // for relations with no properties, attempt to populate with data from the relation layer
+      if (feature.id) {
+        // add OSM ID info as properties for convenience 
+        featureProperties['osm.id'] = osmIdFromBeefsteakFeatureId(feature.id);
+        featureProperties['osm.type'] = osmTypeFromBeefsteakFeatureId(feature.id);
+      }
+      const featureKeys = Object.keys(feature.properties);
 
-              const id = Math.floor(feature.id * 0.1);
-              const relation = relationsById[id];
+      if (feature.id % 10 === 3) { // relation
+        if (featureKeys.length === 0) {
+          // for relations with no properties, attempt to populate with data from the relation layer
 
-              if (relation) {
-                for (const prop in relation.properties) {
-                  feature.properties[prop] = relation.properties[prop];
-                }
-              }
-            }
+          const id = Math.floor(feature.id * 0.1);
+          const relation = relationsById[id];
 
-          } else { // non-relation
-            // add relation tags based on relation properties given in the format `m.{relation_id}={member_role}`
-
-            const linkedRelations = Object.keys(feature.properties)
-              .filter(key => key.startsWith('m.'))
-              .map(key => parseInt(key.substring(2)))
-              .sort((a, b) => a - b)
-              .map(id => relationsById[id])
-              .filter(Boolean);
-
-            if (linkedRelations.length) {
-              for (const key of allRelationKeys) {
-                const values = linkedRelations.map(rel => rel.properties[key]);
-                const joined = '┃' + values.join('┃') + '┃';
-
-                // only add the property if at least one of the relations has a value
-                if (joined.length > linkedRelations.length + 1) {
-                  feature.properties['r.' + key] = joined;
-                }
-              }
+          if (relation) {
+            for (const prop in relation.properties) {
+              featureProperties[prop] = relation.properties[prop];
             }
           }
-          return feature;
+        }
+
+      } else if (allRelationKeysCount) { // non-relation
+        // Based on relation memberships in the form `m.{relation_id}={member_role}`,
+        // add the relation tags to the feature in the form `r.{relation_key}=┃{relation1_value}┃{relation2_value}┃`
+
+        const linkedRelations = [];
+        for (let i = 0; i < featureKeys.length; i+=1) {
+          const key = featureKeys[i];
+          if (key[0] === 'm' && key[1] === '.') {
+            const id = Number(key.slice(2));
+            const rel = relationsById[id];
+            if (rel) linkedRelations.push(rel);
+          }
+        }
+        // we need deterministic ordering so the same relation is always represented as the same index
+        linkedRelations.sort((a, b) => a.id - b.id);
+
+        const linkedRelationsCount = linkedRelations.length;
+
+        if (linkedRelationsCount) {
+
+          for (let i = 0; i < allRelationKeysCount; i+=1) {
+            const key = allRelationKeys[i];
+            let out = '┃';
+            let hasValue = false;
+
+            for (let j = 0; j < linkedRelationsCount; j+=1) {
+              const value = linkedRelations[j].properties[key];
+              if (value != null && value !== '') hasValue = true;
+              out += (value ?? '') + '┃';
+            }
+
+            // only add the property if at least one of the relations has a value
+            if (hasValue) featureProperties['r.' + key] = out;
+          }
         }
       }
-    }), {})
-  };
+      return feature;
+    }
+  }
 }
 
 function osmIdFromBeefsteakFeatureId(id) {
