184e3a21a9cf89d9809edb04b80e0a3ddcc5a10a
Quincy Morgan <2046746+quincylvania@users.noreply.github.com>
2026-01-14 09:27:20 -0500

    Revert "Resume more or less normal operations"
    This reverts commit 203bd6356aa83e6e274ee5d23ed928950cbb408e.

--- server/update_osm_followup.sh
+++ server/update_osm_followup.sh
@@ -20,6 +20,4 @@ duration=$((end - start))
 echo "Duration: $duration seconds"
 
 # script runs its own echos
-# python3 "$SCRIPT_DIR/process_expired_tiles.py"
-# don't worry about tile expiration for now
-rm /var/lib/app/expired_tiles.txt
+python3 "$SCRIPT_DIR/process_expired_tiles.py"
