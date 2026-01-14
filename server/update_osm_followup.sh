00c8e824cd452dcc3c6b954cb30fa2c222db1fa8
Quincy Morgan <2046746+quincylvania@users.noreply.github.com>
2026-01-13 20:04:06 -0500

    Time steps of OSM followup

--- server/update_osm_followup.sh
+++ server/update_osm_followup.sh
@@ -8,10 +8,21 @@ SCRIPT_DIR="$(dirname "$0")"
 
 DB_NAME="osm"
 
+start=$(date +%s)
+echo "Start: $(date '+%Y-%m-%d %H:%M:%S')"
 echo "Running post-import SQL queries..."
 psql "$DB_NAME" \
     --file="$SCRIPT_DIR/sql/post_init_or_update/area_relation.sql"
 echo "Done running post-import SQL queries"
+end=$(date +%s)
+echo "End:   $(date '+%Y-%m-%d %H:%M:%S')"
+duration=$((end - start))
+echo "Duration: $duration seconds"
 
+start=$(date +%s)
+echo "Start: $(date '+%Y-%m-%d %H:%M:%S')"
 # script runs its own echos
 bash "$SCRIPT_DIR/process_expired_tiles.sh"
+echo "End:   $(date '+%Y-%m-%d %H:%M:%S')"
+duration=$((end - start))
+echo "Duration: $duration seconds"
